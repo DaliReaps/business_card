42cf74bd2af27a00fb45167df0f79772c0402c5f
Dali <chaouch.mohammed.ali@gmail.com>
2023-05-17 21:18:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function linkedin(event){
+    window.open("https://www.linkedin.com/in/mohamed-ali-chaouch-773312105/");
+}
+function facebook(event){
+    window.open("https://www.facebook.com/mohamedali.chaouch.37/");
+}
+function twitter(event){
+    window.open("https://twitter.com/xiireaperiix");
+}
+function instagram(event){
+    window.open("https://www.instagram.com/chaouch.mohamedali/");
+}
+function github(event){
+    window.open("https://github.com/DaliReaps");
+}
